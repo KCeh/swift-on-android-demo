a4eb9d1c2d3b6e4735456ad0e82d7faaf34147bc
Karlo Čeh <karlo.ceh@infinum.com>
2025-10-05 16:34:00 +0200

    Add native libraries

--- app/src/main/java/com/karlo/ceh/swiftonandroiddemo/MainActivity.kt
+++ app/src/main/java/com/karlo/ceh/swiftonandroiddemo/MainActivity.kt
@@ -14,9 +14,29 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.karlo.ceh.swiftonandroiddemo.ui.theme.SwiftOnAndroidDemoTheme
 
 class MainActivity : ComponentActivity() {
+    companion object {
+        init {
+            System.loadLibrary("c++_shared")
+            System.loadLibrary("swiftSwiftOnoneSupport")
+            System.loadLibrary("swiftCore")
+            System.loadLibrary("swiftAndroid")
+            System.loadLibrary("swift_Concurrency")
+            System.loadLibrary("dispatch")
+            System.loadLibrary("BlocksRuntime")
+            System.loadLibrary("swift_StringProcessing")
+            System.loadLibrary("swift_RegexParser")
+            System.loadLibrary("HelloWorld")
+        }
+    }
+
+    private external fun printHelloFromSwift()
+
+    // todo improve jni folder naming
+    // todo add buttons for demo scenarios
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        printHelloFromSwift()
         setContent {
             SwiftOnAndroidDemoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
